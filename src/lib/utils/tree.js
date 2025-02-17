0dd8888f9c738f9ba51137fda37328b57f2f2e95
Joe Kelly <joedotkelly@gmail.com>
2025-02-17 15:33:20 +0000

    fix tree

--- src/lib/utils/tree.js
+++ src/lib/utils/tree.js
@@ -64,7 +64,7 @@ export const buildTree = (forks, active_fork, messages, stars, leaf_spacing) => 
         node.row    = node.depth * 2 + 1
         node.column = Math.round(node.x * leaf_spacing) + 1
 
-        const message    = messages[node.id],
+        const message    = messages.find(m => m.id === node.id),
               is_active  = forks[active_fork].message_ids.slice(1).includes(node.id),
               is_starred = stars.includes(node.id)
 
