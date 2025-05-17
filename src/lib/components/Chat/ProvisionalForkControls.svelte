6facc96dad7e5f7311e218beaf920f01d3b4c92b
Joe Kelly <joedotkelly@gmail.com>
2025-05-17 11:05:08 +0100

    provisional fork button styles

--- src/lib/components/Chat/ProvisionalForkControls.svelte
+++ src/lib/components/Chat/ProvisionalForkControls.svelte
@@ -3,7 +3,6 @@
     import { fly } from 'svelte/transition'
     import { quartOut } from 'svelte/easing'
     import { is_hovering } from '$lib/stores/chat/interactions'
-    import { model } from '$lib/stores/ai'
 
     import AddIcon from '$lib/components/Icons/Add.svelte'
     import DeleteIcon from '$lib/components/Icons/Delete.svelte'
@@ -29,90 +28,116 @@
         <div class='icon-container'>
             <AddIcon className='icon' />
         </div>
-        Add Reply
+        <div class='text'>
+            Add Reply
+        </div>
     </button>
     <button class='provisional-fork-button cancel-fork' title='Cancel Fork (esc)' on:click={clickedCancelFork}>
         <div class='icon-container'>
             <DeleteIcon className='icon' />
         </div>
-        Cancel Fork
+        <div class='text'>
+            Cancel Fork
+        </div>
     </button>
 </div>
 
 <style lang='sass'>
     .provisional-fork-controls
-        position:    absolute
-        bottom:      0
-        left:        100%
-        margin-left: space.$default-padding
+        display:        flex
+        flex-direction: column
+        align-items:    flex-start
+        gap:            16px
+        position:       absolute
+        bottom:         0
+        left:           100%
+        margin-left:    space.$default-padding
+        user-select:    none
 
-        :global
-            .provisional-fork-button
-                $button-size:  48px
-                display:       flex
-                align-items:   center
-                position:      relative
-                margin-top:    16px
-                height:        $button-size
-                box-sizing:    border-box
-                padding:       0
-                padding-right: 21px
-                border-radius: 8px
-                border:        1px solid $background-lighter
-                font-size:     16px
-                font-weight:   425
-                color:         $background-lightest
-                white-space:   nowrap
-                cursor:        pointer
+    .provisional-fork-button
+        $button-size:  40px
+        display:       flex
+        align-items:   center
+        height:        $button-size
+        box-sizing:    border-box
+        padding:       0
+        border-radius: 8px
+        border:        1px solid $background-lighter
+        font-size:     14px
+        font-weight:   450
+        color:         $background-lightest
+        white-space:   nowrap
+        cursor:        pointer
 
-                .icon-container
-                    display:         flex
-                    justify-content: center
-                    align-items:     center
-                    width:           $button-size
-                    height:          $button-size
+        .icon-container
+            display:         flex
+            justify-content: center
+            align-items:     center
+            width:           $button-size
+            height:          $button-size
+        
+        .text
+            padding-right: 16px
 
-                .icon
-                    fill: $background-lightest
+        &.add-reply
+            :global(.icon)
+                height:    11px
+                transform: rotate(45deg)
+                fill:      $background-lightest
+
+            &:hover
+                border-color:     $blue
+                background-color: $blue
+                color:            $background-darker
+
+                //  hack/fix: when adding a 6th reply, the forks container
+                //  expands on hover, moving the button down, causing the hover
+                //  to glitch on/off, so we need to compensate for that with
+                //  a run-off area above.  (And to the right, for the edge case
+                //  where the scrollbar is added to the screen by the change
+                //  in height):
+
+                &:before
+                    content:  ''
+                    position: absolute
+                    top:      -58px
+                    left:     0
+                    width:    100%
+                    height:   100px
                 
-                &:hover,
-                &:active
-                    color: $background-darker
+                :global(.icon)
+                    fill: $background-darker
 
-                    .icon
-                        fill: $background-darker
+            &:active
+                border-color:     color.adjust($blue, $lightness: -3%)
+                background-color: color.adjust($blue, $lightness: -3%)
+                color:            $background-darkest
 
-                &.add-reply
-                    .icon
-                        height:    11px
-                        transform: rotate(45deg)
-                    &:hover
-                        border-color:     $blue
-                        background-color: $blue
-                        //  hack/fix: when adding a 6th reply, the forks container
-                        //  expands on hover, moving the button down, causing the hover
-                        //  to glitch on/off, so we need to compensate for that with
-                        //  a run-off area above.  (And to the right, for the edge case
-                        //  where the scrollbar is added to the screen by the change
-                        //  in height):
-                        &:before
-                            content:  ''
-                            position: absolute
-                            top:      -58px
-                            left:     0
-                            width:    100%
-                            height:   100px
-                    &:active
-                        border-color:     color.adjust($blue, $lightness: -3%)
-                        background-color: color.adjust($blue, $lightness: -3%)
+                :global(.icon)
+                    fill: $background-darkest
 
-                &.cancel-fork
-                    .icon
-                        height: 19px
-                    &:hover
-                        border-color:     $coral
-                        background-color: $coral
-                    &:active
-                        border-color:     color.adjust($coral, $lightness: -3%)
-                        background-color: color.adjust($coral, $lightness: -3%)
+        &.cancel-fork
+            border-color:     $coral
+            background-color: $coral
+            color:            $background-darker
+
+            :global(.icon)
+                height: 19px
+                fill:   $background-darker
+
+            &:hover
+                border-color:     color.adjust($coral, $alpha: -0.075)
+                background-color: color.adjust($coral, $alpha: -0.075)
+                color:            $background-darkest
+
+                :global(.icon)
+                    fill: $background-darkest
+
+            &:active
+                border-color:     color.adjust($coral, $alpha: -0.125)
+                background-color: color.adjust($coral, $alpha: -0.125)
+                color:            $background-darkest
+
+                :global(.icon)
+                    fill: $background-darkest
 </style>
