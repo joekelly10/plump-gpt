9403b91e0e6746eaa97afcae3270b7902a9cfecd
Joe Kelly <joedotkelly@gmail.com>
2025-06-12 11:15:08 +0100

    test mistral + ai21 streaming

--- src/routes/api/ai/chat/mistral/+server.js
+++ src/routes/api/ai/chat/mistral/+server.js
@@ -1,6 +1,9 @@
 import { env } from '$env/dynamic/private'
-
-export const POST = async ({ request }) => {
+//
+//  use SvelteKit's `fetch` (as `internal_fetch`) for mock endpoints in tests
+//  use `fetch` for external API calls, otherwise you can get CORS errors
+//
+export const POST = async ({ request, fetch: internal_fetch }) => {
     let { messages, options } = await request.json()
 
     // strip all properties except `role` + `content` else you get a 400
@@ -18,6 +21,17 @@ export const POST = async ({ request }) => {
         stream:      true,
         messages:    messages
     })
+
+    if (env.NODE_ENV === 'test') {
+        //
+        //  Playwright doesn't support streaming endpoints
+        //  so we need to mock this ourselves :/
+        //
+        return internal_fetch('/mock/ai/chat/mistral', {
+            method: 'POST',
+            body
+        })
+    }
 
     return fetch('https://api.mistral.ai/v1/chat/completions', {
         method: 'POST',
