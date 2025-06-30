1c514fa5ce9177793b5849279de51080bfa8a80d
Joe Kelly <joedotkelly@gmail.com>
2025-06-30 10:53:55 +0100

    clip fork animation

--- src/lib/components/Message/HoverInfoFork.svelte
+++ src/lib/components/Message/HoverInfoFork.svelte
@@ -3,22 +3,30 @@
     import { quartOut } from 'svelte/easing'
 </script>
 
-<div class='hover-info-fork' in:fly={{ y: -1.5, duration: 100, easing: quartOut }} out:fly={{ y: -1.5, duration: 75, easing: quartOut }}>
-    <span class='text'>
-        Fork Here
-    </span>
+<div class='hover-info-fork-clipper'>
+    <div class='hover-info-fork' in:fly={{ y: -4, duration: 100, easing: quartOut }} out:fly={{ y: -4, duration: 75, easing: quartOut }}>
+        <span class='text'>
+            Fork Here
+        </span>
+    </div>
 </div>
 
 <style lang='sass'>
+    .hover-info-fork-clipper
+        position: absolute
+        top:      100%
+        left:     0
+        width:    100%
+        height:   200px // any size, just bigger than it needs to be   
+        overflow: hidden
+
     .hover-info-fork
         position:         absolute
-        bottom:           -1.5px
+        top:              0
         right:            0
-        transform:        translateY(100%)
-        z-index:          1000
         display:          flex
         align-items:      center
-        padding:          12px 24px 16px
+        padding:          14.5px 24px 16px
         border-radius:    0 0 8px 8px
         background-color: $blue
         color:            $background-800
